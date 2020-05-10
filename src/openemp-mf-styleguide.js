188e7e82cd82af87975b12b0fd1cb4a0e7925ab4
chinzou <chine.zoheir@gmail.com>
2020-05-11 03:04:40 +0800

    Initial commit

---
+++ src/openemp-mf-styleguide.js
@@ -0,0 +1,5 @@
+import "./set-public-path";
+import "./global.css?modules=false";
+
+// You can also export React components from this file and import them into your microfrontends
+export { default as Button } from "./button.component.js";
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = webpackConfigEnv => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "openemp-mf",
+    projectName: "styleguide",
+    webpackConfigEnv
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: [require("tailwindcss"), require("autoprefixer")]
+              }
+            }
+          ]
+        }
+      ]
+    }
+  });
+};
