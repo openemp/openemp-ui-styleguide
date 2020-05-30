53ed4bfcb33996754b6743c07a2aba37935c9aae
Alaa Eddine Bouasla <bbbooo1994@gmail.com>
2020-05-30 05:21:38 +0100

    Add base app structure (#6)
    * initial application structure
    
    * fix versioning

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,81 @@
-const webpackMerge = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-react");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { UnusedFilesWebpackPlugin } = require('unused-files-webpack-plugin');
+const path = require('path');
 
-module.exports = webpackConfigEnv => {
-  const defaultConfig = singleSpaDefaults({
-    orgName: "openemp-mf",
-    projectName: "styleguide",
-    webpackConfigEnv,
-  });
+module.exports = (webpackConfigEnv = {}) => {
+  const opts = {
+    orgName: 'openemp-mf',
+    projectName: 'styleguide',
+    port: 9002,
+  };
 
-  return webpackMerge.smart(defaultConfig, {
+  return {
+    entry: path.resolve(process.cwd(), `src/${opts.orgName}-${opts.projectName}.js`),
+    output: {
+      filename: `${opts.orgName}-${opts.projectName}.js`,
+      libraryTarget: 'system',
+      path: path.resolve(process.cwd(), 'dist'),
+      jsonpFunction: `webpackJsonp_${opts.projectName}`,
+    },
     module: {
       rules: [
         {
+          parser: {
+            system: false,
+          },
+        },
+        {
+          test: /\.(js|ts)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
           test: /\.css$/i,
+          include: [/node_modules/, /src/],
           use: [
-            "style-loader",
-            "css-loader",
+            { loader: 'style-loader' },
             {
-              loader: "postcss-loader",
+              loader: 'css-loader',
               options: {
-                ident: "postcss",
-                plugins: [require("tailwindcss"), require("autoprefixer")],
+                modules: false,
               },
             },
           ],
         },
       ],
     },
+    devtool: 'source-map',
     devServer: {
-      port: 9002,
+      compress: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      disableHostCheck: true,
+      port: opts.port,
     },
-  });
+    externals: ['react', 'react-dom', 'single-spa', new RegExp(`^@${opts.orgName}/`)],
+    plugins: [
+      new CleanWebpackPlugin(),
+      new BundleAnalyzerPlugin({
+        analyzerMode: webpackConfigEnv.analyze ? 'server' : 'disabled',
+      }),
+      new UnusedFilesWebpackPlugin({
+        globOptions: {
+          cwd: path.resolve(process.cwd(), 'src'),
+          ignore: ['**/*.test.*', '**/*.spec.*', '**/*.*.snap', '**/test-setup.*', '**/*.stories.*'],
+        },
+      }),
+    ],
+    resolve: {
+      extensions: ['.js', '.mjs', '.jsx', '.wasm', '.json'],
+      alias: {
+        assets: path.resolve(__dirname, 'src/assets'),
+        components: path.resolve(__dirname, 'src/components'),
+        themes: path.resolve(__dirname, 'src/themes'),
+      },
+    },
+  };
 };
