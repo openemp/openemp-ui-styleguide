1f7dbf4cf38935f17a88550adea3a66d1364737e
honohunter <bbbooo1994@gmail.com>
2020-06-26 14:06:17 +0100

    Add rtl support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './set-public-path';
 import * as yup from 'yup';
+import { create as createJss } from 'jss';
+import rtl from 'jss-rtl';
 
 export * from '@material-ui/core';
 export * from 'formik';
 export { TextField as TextFieldFormik } from 'formik-material-ui';
-export { yup };
+export { yup, createJss, rtl };
